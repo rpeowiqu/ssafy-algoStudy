3574781f883b7c2e4c0f6793940f2b3ff1d71893
JaeseoHan <hanahyun1@naver.com>
2024-07-29 14:34:49 +0900

    Done to 2007

---
+++ src/sepFourth/Main2007.java
@@ -0,0 +1,40 @@
+package sepFourth;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+
+public class Main2007 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int testCase = Integer.parseInt(br.readLine());
+		for(int t=0; t < testCase; t++) {
+			String wordRepeat = br.readLine();
+			boolean end = false;
+			for(int i=1; i < 30; i++) {
+				if(wordRepeat.charAt(i) == wordRepeat.charAt(0)) {
+					if(i < 16 && wordRepeat.substring(0, i).equals(wordRepeat.substring(i, 2 * i))) {
+						bw.write("#" + (t + 1) + " " + i + "\n");
+						end = true;
+						break;
+					} else if(wordRepeat.substring(0, 30 - i).equals(wordRepeat.substring(i, 30))) {
+						bw.write("#" + (t + 1) + " " + i + "\n");
+						end = true;
+						break;
+					}
+				}
+			}
+			
+			if(!end) {
+				bw.write("#" + (t + 1) + " " + 30 + "\n");
+			}
+		}
+		
+		bw.flush();
+		bw.close();
+	}
+}
